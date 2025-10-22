0b199267c13ad29282fd8baaf24d4e54566f0392
Matheus-OAMK <t2fema00@students.oamk.fi>
2025-10-22 17:01:50 +0300

    replace webdev-icons with mini-icons

--- config/nvim/lua/config/plugins/blink-cmp.lua
+++ config/nvim/lua/config/plugins/blink-cmp.lua
@@ -1,11 +1,12 @@
 return {
   {
     'saghen/blink.cmp',
+    version = '1.*',
     -- optional: provides snippets for the snippet source
     dependencies = {
       'rafamadriz/friendly-snippets',
       "onsails/lspkind.nvim", -- vs code like pictogram
-      "nvim-tree/nvim-web-devicons", -- optional, for file icons
+      "nvim-mini/mini.icons", -- Optional, for file icons
     },
 
     opts = {
@@ -14,55 +15,53 @@ return {
       appearance = {
         nerd_font_variant = 'mono'
       },
-
       completion = {
         documentation = { auto_show = true, auto_show_delay_ms = 250  },
-        ghost_text= {enabled = true},
+        ghost_text= { enabled = true },
         menu = {
           draw = {
             components = {
               kind_icon = {
                 text = function(ctx)
-                  local icon = ctx.kind_icon
                   if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                    if dev_icon then
-                      icon = dev_icon
-                    end
-                  else
-                    icon = require("lspkind").symbolic(ctx.kind, {
-                      mode = "symbol",
-                    })
+                    local mini_icon, _ = require("mini.icons").get_icon(ctx.item.data.type, ctx.label)
+                    if mini_icon then return mini_icon .. ctx.icon_gap end
                   end
 
+                  local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
                   return icon .. ctx.icon_gap
                 end,
 
-                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- Optionally, use the highlight groups from mini.icons
                 -- You can also add the same function for `kind.highlight` if you want to
                 -- keep the highlight groups in sync with the icons.
                 highlight = function(ctx)
-                  local hl = ctx.kind_hl
                   if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                    if dev_icon then
-                      hl = dev_hl
-                    end
+                    local mini_icon, mini_hl = require("mini.icons").get_icon(ctx.item.data.type, ctx.label)
+                    if mini_icon then return mini_hl end
                   end
-                  return hl
+                  return ctx.kind_hl
+                end,
+              },
+              kind = {
+                -- Optional, use highlights from mini.icons
+                highlight = function(ctx)
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local mini_icon, mini_hl = require("mini.icons").get_icon(ctx.item.data.type, ctx.label)
+                    if mini_icon then return mini_hl end
+                  end
+                  return ctx.kind_hl
                 end,
               }
             }
           }
         }
-
       },
-
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
 
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      fuzzy = { implementation = "rust" }
     },
     opts_extend = { "sources.default" }
   }
