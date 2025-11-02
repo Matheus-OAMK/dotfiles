4904de9bfd50035fe126f16509c3a3e65fd8a1d2
Matheus-OAMK <t2fema00@students.oamk.fi>
2025-11-03 01:19:56 +0200

    nvim: Add bufferline

---
+++ config/nvim/lua/config/plugins/bufferline.lua
@@ -0,0 +1,52 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-mini/mini.icons" }, -- Optional, for file icons 
+  event = "VeryLazy",
+  opts = {
+    options = {
+
+      diagnostics = "nvim_lsp",
+
+      show_buffer_close_icons = false,
+
+      indicator = {
+        icon = "▎", -- this should be omitted if indicator style is not 'icon'
+        style = "none",
+      },
+
+
+      diagnostics_indicator = function(_, _, diag)
+        local icons = {
+          Error = "󰅚 ",
+          Warn = "󰀪 ",
+          Info = "󰋽 ",
+          Hint = "󰌶 ",
+        }
+        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+        .. (diag.warning and icons.Warn .. diag.warning or "")
+        return vim.trim(ret)
+      end,
+
+      separator_style = "thick",
+
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "File Explorer",
+          highlight = "Directory",
+          separator = true, -- use a "true" to enable the default, or set your own character
+          text_align = "left",
+        },
+      },
+    },
+  },
+
+  keys = {
+    { "<leader>bP", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+    { "<leader>bX", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    { "<leader>bp", "<cmd>BufferLineCyclePrev<cr>", desc = "Go to prev Buffer" },
+    { "<leader>bn", "<cmd>BufferLineCycleNext<cr>", desc = "Go to next Buffer" },
+  },
+}
