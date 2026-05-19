4d909a824cd5e1743953d1d0113dfda3d2d68d9c
Matheus-OAMK <t2fema00@students.oamk.fi>
2026-05-19 18:41:07 +0300

    nvim update gitsigns keybind and mini pairs for unbalanced symbols

--- config/nvim/lua/config/plugins/gitsigns.lua
+++ config/nvim/lua/config/plugins/gitsigns.lua
@@ -1,27 +1,28 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "" },
-			topdelete = { text = "" },
-			changedelete = { text = "▎" },
-			untracked = { text = "▎" },
-		},
-		signs_staged = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "" },
-			topdelete = { text = "" },
-			changedelete = { text = "▎" },
-		},
-		on_attach = function(buffer)
-			local gs = package.loaded.gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+			signs_staged = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+			},
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
 
-			local function map(mode, l, r, desc)
-				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
-			end
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
+				end
 
       -- stylua: ignore start
       map("n", "]h", function()
@@ -51,6 +52,22 @@ return {
       map("n", "<leader>ghd", gs.diffthis, "Diff This")
       map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+			end,
+		},
+	},
+
+	{
+		"gitsigns.nvim",
+		opts = function()
+			Snacks.toggle({
+				name = "Git Signs",
+				get = function()
+					return require("gitsigns.config").config.signcolumn
+				end,
+				set = function(state)
+					require("gitsigns").toggle_signs(state)
+				end,
+			}):map("<leader>uG")
 		end,
 	},
 }
