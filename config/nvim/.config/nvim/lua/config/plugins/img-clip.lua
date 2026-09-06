572e55f05a93ca0a96689beb085f19d6332d76f9
Matheus-OAMK <t2fema00@students.oamk.fi>
2026-09-07 00:42:06 +0300

    update

---
+++ config/nvim/.config/nvim/lua/config/plugins/img-clip.lua
@@ -0,0 +1,16 @@
+return {
+	"HakonHarnes/img-clip.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add options here
+		-- or leave it empty to use the default settings
+		default = {
+			relative_to_current_file = true,
+			file_name = "%d%m%y-%H%M%S",
+		},
+	},
+	keys = {
+		-- suggested keymap
+		{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
+}
