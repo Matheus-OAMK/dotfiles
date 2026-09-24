fd2f022375793e51f86bcea551bc4f05b5367360
Matheus-OAMK <github@mroliveira.net>
2026-09-24 12:30:39 +0000

    sensitivity increase

--- config/hypr/.config/hypr/conf/input.lua
+++ config/hypr/.config/hypr/conf/input.lua
@@ -8,7 +8,7 @@ hl.config({
 
 		follow_mouse = 1,
 
-		sensitivity = -0.3, -- -1.0 - 1.0, 0 means no modification.
+		sensitivity = 0.2, -- -1.0 - 1.0, 0 means no modification.
 		accel_profile = "flat", -- disable accel
 
 		touchpad = {
